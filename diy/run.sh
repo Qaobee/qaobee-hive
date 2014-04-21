a44a612f263de7d64f408137aee742e7e603802f
Xavier MARIN <marin.xavier@gmail.com>
2014-04-21 09:28:35 +0200

    scripting

---
+++ diy/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ARGS=
+PROD_ARGS=-d
+
+if [ "x$1" = "x" ]; then
+	echo 'You must declare environnement arg (PROD/DEV)' 
+	exit 1
+fi
+
+if [ "x$1" = "xPROD" ]; then
+	ARGS="$ARGS $PROD_ARGS" 
+fi
+
+echo Copying config files from $1 ENV
+cp -f $OPENSHIFT_REPO_DIR/diy/elasticsearch/config/$1/* $OPENSHIFT_REPO_DIR/diy/elasticsearch/config/. 
+echo running ES with args : $ARGS
+$OPENSHIFT_REPO_DIR/diy/elasticsearch/bin/elasticsearch $ARGS
