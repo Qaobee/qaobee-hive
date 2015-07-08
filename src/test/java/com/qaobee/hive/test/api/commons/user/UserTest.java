981d54a7059208a1cdc8f6b9a7f67aa0c2afd391
Xavier MARIN <marin.xavier@gmail.com>
2015-07-09 00:27:57 +0200

    bug fix

--- src/test/java/com/qaobee/hive/test/api/commons/user/UserTest.java
+++ src/test/java/com/qaobee/hive/test/api/commons/user/UserTest.java
@@ -374,7 +374,7 @@ public class UserTest extends VertxJunitSupport {
         Assert.assertEquals(u.getName(), result.getString("name"));
         req.setBody(null);
         JsonObject reply = new JsonObject(sendonBus(UserVerticle.LOGIN_BY_TOKEN, req));
-        Assert.assertTrue("loginByMobileTokenNoData", reply.getString("code").contains(ExceptionCodes.INVALID_PARAMETER.toString()));
+        Assert.assertTrue("loginByMobileTokenNoData", reply.getString("code").contains(ExceptionCodes.HTTP_ERROR.toString()));
     }
 
     /**
