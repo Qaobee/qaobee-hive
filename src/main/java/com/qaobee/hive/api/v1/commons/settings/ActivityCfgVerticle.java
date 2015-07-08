9b352e006e10aab87f2e4d0f3701769461a8bb63
Xavier MARIN <marin.xavier@gmail.com>
2015-07-08 22:47:49 +0200

    ActivityCfg

---
+++ src/main/java/com/qaobee/hive/api/v1/commons/settings/ActivityCfgVerticle.java
@@ -0,0 +1,107 @@
+/*
+ * __________________
+ *   Qaobee
+ * __________________
+ *
+ *  Copyright (c) 2015.  Qaobee
+ *  All Rights Reserved.
+ *
+ *  NOTICE: All information contained here is, and remains
+ *  the property of Qaobee and its suppliers,
+ *  if any. The intellectual and technical concepts contained
+ *  here are proprietary to Qaobee and its suppliers and may
+ *  be covered by U.S. and Foreign Patents, patents in process,
+ *  and are protected by trade secret or copyright law.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from Qaobee.
+ */
+
+package com.qaobee.hive.api.v1.commons.settings;
+
+import com.qaobee.hive.api.v1.Module;
+import com.qaobee.hive.business.model.commons.settings.ActivityCfg;
+import com.qaobee.hive.technical.annotations.DeployableVerticle;
+import com.qaobee.hive.technical.constantes.Constantes;
+import com.qaobee.hive.technical.exceptions.ExceptionCodes;
+import com.qaobee.hive.technical.exceptions.QaobeeException;
+import com.qaobee.hive.technical.mongo.MongoDB;
+import com.qaobee.hive.technical.utils.Utils;
+import com.qaobee.hive.technical.utils.guice.AbstractGuiceVerticle;
+import com.qaobee.hive.technical.vertx.RequestWrapper;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.json.impl.Json;
+
+import javax.inject.Inject;
+
+/**
+ * The type Activity cfg verticle.
+ */
+@DeployableVerticle(isWorker = true)
+public class ActivityCfgVerticle extends AbstractGuiceVerticle {
+    /**
+     * The constant GET.
+     */
+    public static final String GET = Module.VERSION + ".commons.settings.activitycfg.get";
+    /**
+     * The constant PARAM_ACTIVITY_ID.
+     */
+    public static final String PARAM_ACTIVITY_ID = "activityId";
+    /**
+     * The Mongo.
+     */
+    @Inject
+    private MongoDB mongo;
+    /**
+     * The Utils.
+     */
+    @Inject
+    private Utils utils;
+
+    /**
+     * Start void.
+     */
+    @Override
+    public void start() {
+        super.start();
+        container.logger().debug(this.getClass().getName() + " started");
+
+        /**
+         * @apiDescription Fetch ActivityCfg
+         * @api {post} /api/1/commons/settings/activitycfg/get Get ActivityCfg
+         * @apiName getById
+         * @apiGroup ActivityCfg API
+         * @apiParam {String} activityId Activity Id
+         * @apiError HTTP_ERROR wrong request method
+         * @apiError NOT_LOGGED invalid token
+         * @apiError INVALID_PARAMETER wrong parameters
+         */
+        vertx.eventBus().registerHandler(GET, new Handler<Message<String>>() {
+            @Override
+            public void handle(Message<String> message) {
+                try {
+                    final RequestWrapper req = Json.decodeValue(message.body(), RequestWrapper.class);
+                    utils.testHTTPMetod(Constantes.POST, req.getMethod());
+                    utils.isUserLogged(req);
+                    JsonObject params = new JsonObject(req.getBody());
+                    utils.testMandatoryParams(params.toMap(), PARAM_ACTIVITY_ID);
+                    message.reply(mongo.getById(params.getString(PARAM_ACTIVITY_ID), ActivityCfg.class));
+                } catch (final NoSuchMethodException e) {
+                    container.logger().error(e.getMessage(), e);
+                    utils.sendError(message, ExceptionCodes.HTTP_ERROR, e.getMessage());
+                } catch (final IllegalArgumentException e) {
+                    container.logger().error(e.getMessage(), e);
+                    utils.sendError(message, ExceptionCodes.INVALID_PARAMETER, e.getMessage());
+                } catch (QaobeeException e) {
+                    container.logger().error(e.getMessage(), e);
+                    utils.sendError(message, e);
+                } catch (Exception e) {
+                    container.logger().error(e.getMessage(), e);
+                    utils.sendError(message, ExceptionCodes.INTERNAL_ERROR, e.getMessage());
+                }
+            }
+        });
+    }
+}
