5538faf840409381ff86adff3b8f575a8eb02a94
Xavier MARIN <marin.xavier@gmail.com>
2016-01-08 22:59:57 +0100

    Docker

--- scripts/mongo/run.sh
+++ scripts/mongo/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 27018:27017 -p 28018:28017 -v /opt/qaobee-hive-db/:/data/db qaobee-hive-db
+docker run -d -p 27018:27017 -p 28018:28017 -v /opt/qaobee-hive-db/:/data/db qaobee-hive-db
