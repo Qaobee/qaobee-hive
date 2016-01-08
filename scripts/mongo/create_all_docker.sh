af0f0218a361dff8fe633c2c18f59ae7ad5dd37c
Xavier MARIN <marin.xavier@gmail.com>
2016-01-08 23:33:08 +0100

    docker

--- scripts/mongo/create_all_docker.sh
+++ scripts/mongo/create_all_docker.sh
@@ -5,7 +5,7 @@ echo 'Script de creation des collections dans la base Hive'
 echo '    ---> Docker edition                             '
 echo '****************************************************'
 
-export db="hive -u hive -p hive --verbose --port $1"
+export db="hive -u hive -p hive --verbose --host $1 --port $2"
 echo ' '
 echo '1) Suppression de toutes les collections'
 mongo ${db} --eval "db.getCollectionNames().forEach(function(c) { if (c.indexOf(\"system.\") == -1) db[c].drop(); })"
