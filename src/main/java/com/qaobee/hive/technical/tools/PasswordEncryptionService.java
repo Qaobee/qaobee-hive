b7f15ad077e33f0af5fbeb3108dbf4243a66a826
Xavier MARIN <marin.xavier@gmail.com>
2016-02-11 00:27:42 +0100

    sonar

--- src/main/java/com/qaobee/hive/technical/tools/PasswordEncryptionService.java
+++ src/main/java/com/qaobee/hive/technical/tools/PasswordEncryptionService.java
@@ -26,44 +26,34 @@ import java.security.spec.InvalidKeySpecException;
  * Created by xavier on 09/11/14.
  */
 public interface PasswordEncryptionService {
-	/**
-	 * Authenticate.
-	 *
-	 * @param attemptedPassword
-	 *            the attempted password
-	 * @param encryptedPassword
-	 *            the encrypted password
-	 * @param salt
-	 *            the salt
-	 * @return true, if successful
-	 * @throws java.security.NoSuchAlgorithmException
-	 *             the no such algorithm exception
-	 * @throws java.security.spec.InvalidKeySpecException
-	 *             the invalid key spec exception
-	 */
-	boolean authenticate(String attemptedPassword, byte[] encryptedPassword, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException;
+    /**
+     * Authenticate.
+     *
+     * @param attemptedPassword the attempted password
+     * @param encryptedPassword the encrypted password
+     * @param salt              the salt
+     * @return true, if successful
+     * @throws InvalidKeySpecException  the invalid key spec exception
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     */
+    boolean authenticate(String attemptedPassword, byte[] encryptedPassword, byte[] salt) throws InvalidKeySpecException, NoSuchAlgorithmException;
 
-	/**
-	 * Gets the encrypted password.
-	 *
-	 * @param password
-	 *            the password
-	 * @param salt
-	 *            the salt
-	 * @return the encrypted password
-	 * @throws java.security.NoSuchAlgorithmException
-	 *             the no such algorithm exception
-	 * @throws java.security.spec.InvalidKeySpecException
-	 *             the invalid key spec exception
-	 */
-	byte[] getEncryptedPassword(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException;
+    /**
+     * Gets the encrypted password.
+     *
+     * @param password the password
+     * @param salt     the salt
+     * @return the encrypted password
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     * @throws InvalidKeySpecException  the invalid key spec exception
+     */
+    byte[] getEncryptedPassword(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException;
 
-	/**
-	 * Generate salt.
-	 *
-	 * @return the byte[]
-	 * @throws java.security.NoSuchAlgorithmException
-	 *             the no such algorithm exception
-	 */
-	byte[] generateSalt() throws NoSuchAlgorithmException;
+    /**
+     * Generate salt.
+     *
+     * @return the byte[]
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     */
+    byte[] generateSalt() throws NoSuchAlgorithmException;
 }
