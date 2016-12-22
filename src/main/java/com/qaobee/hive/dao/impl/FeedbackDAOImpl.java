799c863c5c46d7afc65ed6f99ebe0242133e001f
Xavier MARIN <marin.xavier@gmail.com>
2016-12-22 20:35:24 +0100

    rebranchement jira

--- src/main/java/com/qaobee/hive/dao/impl/FeedbackDAOImpl.java
+++ src/main/java/com/qaobee/hive/dao/impl/FeedbackDAOImpl.java
@@ -20,15 +20,21 @@
 package com.qaobee.hive.dao.impl;
 
 import com.qaobee.hive.dao.FeedbackDAO;
+import com.qaobee.hive.technical.exceptions.ExceptionCodes;
 import com.qaobee.hive.technical.exceptions.QaobeeException;
+import net.rcarz.jiraclient.*;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vertx.java.core.json.JsonObject;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-
-//import net.rcarz.jiraclient.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The type Feedback dao.
@@ -41,7 +47,7 @@ public class FeedbackDAOImpl implements FeedbackDAO {
 
     @Override
     public void sendFeedback(JsonObject data) throws QaobeeException {
-      /*  BasicCredentials creds = new BasicCredentials(config.getString("user"), config.getString("passwd"));
+        BasicCredentials creds = new BasicCredentials(config.getString("user"), config.getString("passwd"));
         JiraClient jira = new JiraClient(config.getString("url"), creds);
         try {
             byte[] img = Base64.decodeBase64(data.getString("img").replace("data:image/png;base64,", ""));
@@ -64,6 +70,6 @@ public class FeedbackDAOImpl implements FeedbackDAO {
         } catch (JiraException | IOException e) {
             LOG.error(e.getMessage(), e);
             throw new QaobeeException(ExceptionCodes.INTERNAL_ERROR, e.getMessage());
-        } */
+        }
     }
 }
