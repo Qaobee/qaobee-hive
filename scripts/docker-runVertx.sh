eff883122e02a276d221043d90b6499a1f2ac23a
Xavier MARIN <marin.xavier@gmail.com>
2017-03-09 23:32:22 +0100

    Bug fix enthropy

--- scripts/docker-runVertx.sh
+++ scripts/docker-runVertx.sh
@@ -2,4 +2,4 @@
 echo "-------------------- $ENV"
 vertx runzip hive-0.1.zip -instances 1 -conf /home/qaobee/${ENV}-conf.json \
 -Dorg.vertx.logger-delegate-factory-class-name=org.vertx.java.core.logging.impl.SLF4JLogDelegateFactory \
--XX:PermSize=128m -XX:MaxPermSize=256m -Xms512m -Xmx1g -XX:+UseParallelGC -Djava.security.egd=file:/dev/urandom
+-XX:PermSize=128m -XX:MaxPermSize=256m -Xms512m -Xmx1g -XX:+UseParallelGC -Djava.security.egd=file:/dev/./urandom
