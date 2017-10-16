921a9b1f39eedf8a7dcf47b8420e633136189d0f
Xavier MARIN <marin.xavier@gmail.com>
2017-10-16 23:27:50 +0200

    bug fix introspection

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ docker run -ti \
 -e OPENSHIFT_MONGODB_DB_PORT=27017 \
 -e OPENSHIFT_MONGODB_DB_PASSWORD=hive \
 -e OPENSHIFT_MONGODB_DB_USERNAME=hive \
+-e OPENSHIFT_DATA_DIR=/opt/hive-data \
 -e ENV=REC \
 --add-host=mongo:${LOCALHOST} \
 -p 8888:8888 \
